dd5b48fc14e0150fcc484d07e4d7acaaa9e65859
nara <parknara0213@gmail.com>
2024-03-05 07:31:35 +0900

    240305 monthlyToDoRouter

---
+++ Server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+
+const monthlyRoutes = require("./routes/monthlyToDoRouter");
+
+const app = express();
+
+const PORT = process.env.port || 3000;
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.log(err));
+
+app.use(monthlyRoutes);
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
